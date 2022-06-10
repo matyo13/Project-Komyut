40c3d0af396f08f4fe7f987b40d236303d9e5537
KeentX <kpcatipay@up.edu.ph>
2022-06-11 02:06:56 +0800

    kent commiting

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-script
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+    fs.readFile('JEEP.txt', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        console.log(data);
+        res.end();
+    });
+}).listen(8080);
