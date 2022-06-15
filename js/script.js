08e10c451f2746cc3de55d21863053b0bfb477b9
KeentX <kpcatipay@up.edu.ph>
2022-06-15 10:35:36 +0800

    Commit #42

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 $(document).ready(function () {
     $("#getDistance").submit(function (e) {
         e.preventDefault();
-        document.getElementById("destination_container").innerText = document.getElementById("destination").value;
+        document.getElementById("destination_container").append(document.getElementById("destination").value);
     })
-})
 
-// var http = require('http');
-// var fs = require('fs');
-// http.createServer(function (req, res) {
-//     fs.readFile('JEEP.txt', function(err, data) {
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.write(data);
-//         console.log(data);
-//         res.end();
-//     });
-// }).listen(8080);
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "../jeep.txt", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+            }
+            var data = allText.split("\n");
+            for(var i = 0; i < data.length; i++) {
+                var subdata = data[i].split(":");
+                for(var j = 0; j < subdata.length; j++ ) {
+                    document.getElementById("data_here").append(subdata[j]);
+                }
+            }
+        }
+    }
+    rawFile.send(null);
+})
