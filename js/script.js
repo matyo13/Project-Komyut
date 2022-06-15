5d3a68f13703be19d3961666241fb76a77ceb086
KeentX <kpcatipay@up.edu.ph>
2022-06-15 09:38:10 +0800

    HTML HAHA

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    fs.readFile('JEEP.txt', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        console.log(data);
-        res.end();
-    });
-}).listen(8080);
+$(document).ready(function () {
+    $("#getDistance").submit(function (e) {
+        e.preventDefault();
+        document.getElementById("destination_container").innerText = document.getElementById("destination").value;
+    })
+})
+
+// var http = require('http');
+// var fs = require('fs');
+// http.createServer(function (req, res) {
+//     fs.readFile('JEEP.txt', function(err, data) {
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//         console.log(data);
+//         res.end();
+//     });
+// }).listen(8080);
