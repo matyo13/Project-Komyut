5d65e7394f420b7d50a36cd8506df981ff3d6c2d
KeentX <kpcatipay@up.edu.ph>
2022-06-16 11:32:27 +0800

    Complete logic for getting all distance from different routes back and fourth

--- js/linked-list.js
+++ js/linked-list.js
@@ -1,50 +1,49 @@
 //Implement linked list
-class Node{
-    constructor(val){
-        this.val = val;
+class Node {
+    constructor(location, distance){
+        this.location = location;
+        this.distance = distance;
         this.next = null; 
     }
 }
 
-class linkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+class linkedList {
+    constructor(head = null){
+        this.head = head;
     }
 }
-
-function insertNode(val) {
-    this.length++;
-    let node = new Node(val);
-
-    if(this.tail){
-        this.tail.next = node;
-        this.tail = node;
-        return node;
-    }
-
-    this.head = this.tail = node;
-    return node;
-}
-
-function deleteNode() {
-    if(this.tail){
-        this.length--;
-
-        const tailNode = this.tail;
-        let currentNode = this.head;
-
-        while(currentNode != tailNode){
-            currentNode = currentNode.next;
-        }
-
-        const beforeTail = currentNode;
-        this.tail = beforeTail;
-        this.tail.next = null;
-
-        return tailNode;
-    }
-
-    return null;
-}
+//
+// function insertNode(location, distance) {
+//     this.length++;
+//     let node = new Node(location, distance);
+//
+//     if(this.tail){
+//         this.tail.next = node;
+//         this.tail = node;
+//         return node;
+//     }
+//
+//     this.head = this.tail = node;
+//     return node;
+// }
+//
+// function deleteNode() {
+//     if(this.tail){
+//         this.length--;
+//
+//         const tailNode = this.tail;
+//         let currentNode = this.head;
+//
+//         while(currentNode != tailNode){
+//             currentNode = currentNode.next;
+//         }
+//
+//         const beforeTail = currentNode;
+//         this.tail = beforeTail;
+//         this.tail.next = null;
+//
+//         return tailNode;
+//     }
+//
+//     return null;
+// }
--- js/script.js
+++ js/script.js
@@ -1,24 +1,105 @@
-$(document).ready(function () {
-    $("#getDistance").submit(function (e) {
-        e.preventDefault();
-        document.getElementById("destination_container").append(document.getElementById("destination").value);
+jQuery(function ($) {
+    const routes = populateRoutes();
+    const button = $("#submit");
+    let possible_routes = [];
+
+    button.attr("disabled", "disabled")
+    button.on("click", function () {
+        let location = $('#location').find(":selected").val();
+        let destination = $('#destination').find(":selected").val();
+        possible_routes.push(getRoutes(routes, location, destination));
+        console.log(possible_routes);
     });
 
+    $("#location, #destination").on("change", function () {
+        if(($('#location').find(":selected").val() !== "0") && ($('#destination').find(":selected").val() !== "0")) {
+            button.removeAttr("disabled");
+        }
+    });
+});
+
+function getRoutes(routes, location, destination) {
+    let distance = 0;
+    let new_route = [];
+    routes.forEach(function(route) {
+        new_route = [];
+        distance = 0;
+        let startCounting = false;
+        let current = route[1].head;
+        new_route.push(route[0]);
+        while(current.next != null) {
+            if(location === current.location) {
+                startCounting = true;
+            }
+
+            if(startCounting) {
+                if(destination === current.location)
+                    break;
+                distance += current.distance;
+            }
+            current = current.next;
+        }
+
+        if(current.next == null) {
+            distance = 0;
+            current = route[1].head;
+            startCounting = false;
+            while(current.next != null) {
+                if(destination === current.location) {
+                    startCounting = true;
+                }
+
+                if(startCounting) {
+                    if(location === current.location)
+                        break;
+                    distance += current.distance;
+                }
+                current = current.next;
+            }
+        }
+        new_route.push(distance, location, destination);
+        console.log(new_route);
+    });
+    return new_route;
+}
+
+function populateRoutes() {
     const routes = getFileData("../jeep.txt").split("\n");
     const places = getFileData("../places.txt").split("\n");
 
     populateOptions(places);
 
-});
+    let linked_routes = []
+    routes.forEach(function(route) {
+        let new_route = new Node();
+        let temp = new_route;
+        let current_route = [];
+        current_route.push(route.split(":")[0]);
+        route.split(":")[1].split("-").forEach(function(place) {
+            temp.location = place.split("?")[0]
+            temp.distance = parseInt(place.split("?")[1]);
+            temp.next = new Node();
+            temp = temp.next;
+        });
+        let new_list = new linkedList(new_route);
+        current_route.push(new_list);
+        linked_routes.push(current_route);
+    });
+
+    return linked_routes;
+}
 
 function populateOptions(places) {
-    places.forEach(function(place) {
-        const option = document.createElement("option");
-        option.text = place;
-        option.value = place;
-        document.getElementById("location").add(option);
-        document.getElementById("destination").add(option);
-    });
+    for(let i = 0; i < places.length; i++) {
+        let location = document.createElement("option");
+        let destination = document.createElement("option");
+        location.text = places[i];
+        location.value = location.text
+        destination.text = places[i];
+        destination.value = destination.text
+        $("#destination").append(destination);
+        $("#location").append(location);
+    }
 }
 
 
@@ -31,7 +112,6 @@ function getFileData(address) {
             if(rawFile.status === 200 || rawFile.status === 0) {
                 allText = rawFile.responseText;
             }
-            console.log(allText);
         }
     }
     rawFile.send(null);
